c47a84a0418a3f413338d5addfa3480a83372bdd
asitball <asit3310@gmail.com>
2021-06-18 17:32:12 +0600

    code push

---
+++ src/components/GoogleMap/GoogleMap.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+
+
+const GoogleMap = () => {
+    const location = {
+        lat: 59.334591,
+        lng: 18.063240,
+    }
+    return (
+        <div style={{ height: '70vh', width: '100%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyDUNxhqkILD9IOW7WszAdRpGbIRNVt-xSY" }}
+                defaultCenter={location}
+                defaultZoom={10}
+            >
+            </GoogleMapReact>
+        </div>
+    );
+};
+
+export default GoogleMap;
--- src/components/Pick/Pick.js
+++ src/components/Pick/Pick.js
@@ -1,35 +1,122 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-
+import rideData from '../../data/data.json';
+import GoogleMap from '../GoogleMap/GoogleMap.js';
 const Pick = () => {
-    const { rideType } = useParams();
-    
+    const {id} = useParams();
+    const [destination, setDestination] = useState({from: '', to: '', date: ''})
+
+    const handleBlur = (event) =>{
+
+        console.log(event.target.name, event.target.value);
+
+        if (event.target.name === 'from') {
+            destination.from = event.target.value;
+        }
+        if (event.target.name === 'to') {
+            destination.to = event.target.value;
+        }
+        if (event.target.name === 'date') {
+            destination.date = event.target.value;
+        }
+        if (event.target.name === 'time') {
+            destination.time = event.target.value;
+        }
+
+    };
+
+    const [search, setSearch] = useState(false);
+    const searchHandler = (event) =>{
+        setSearch(!search);
+        event.preventDefault();
+    }
+    const [vehicle, setVehicle] = useState([]);
+
+    useEffect(() =>{
+        const info = rideData.filter((type)=>id === type.id)
+        setVehicle(info[0]);
+    },[id])
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-5">
-                    <h1>Let's pick a {rideType}</h1>
-                    <p>want a <Link to='/home'>different Ride</Link></p>
-                    <div class="mb-3">
-                        <label for="formGroupExampleInput" class="form-label"> Pick from</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Starting location"/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="formGroupExampleInput2" class="form-label">Pick to</label>
-                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ending location"/>
-                            <br/>
-                            <button className='btn btn-primary'>Search</button>
-                        </div>
-                        </div>
-                        <div className="col-md-7">
-                            <h3>hello</h3>
-                        </div>
+        <div className='container py-3'>
+            <div className='row row-cols-1 row-cols-md-3 g-4'>
+                <div className='col-lg-5'>
+                    <div className='bg-dark p-3 rounded text-white text-center'>
+                        <h2>{} Ride</h2>
                     </div>
+                    <br />
+                    {!search ? (
+                        <div className='bg-info p-3 rounded'>
+                            <form onSubmit={searchHandler}>
+                                <div className='form-group'>
+                                    <label htmlFor='exampleInputEmail1'>Pick From</label>
+                                    <input type='text' className='form-control' placeholder='From' name='from' onBlur={handleBlur} required/>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='exampleInputPassword1'>Pick To</label>
+                                    <input type='text' className='form-control' placeholder='To' name='to' onBlur={handleBlur} required/>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='exampleInputPassword1'>Date</label>
+                                    <input type='date' className='form-control' placeholder='To' name='date' onBlur={handleBlur} required/>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='exampleInputPassword1'>Time</label>
+                                    <input type='time' className='form-control' placeholder='To' name='time' onBlur={handleBlur} required/>
+                                </div>
+                                <input type='submit' className='btn btn-primary form-control' value='Search'/>
+                            </form>
+                        </div>
+                    ) : (
+                        <div className='bg-primary p-3 rounded'>
+                           
+                            <table className='table table-bordered table-hover bg-white'>
+                                <tbody>
+                                    <tr>
+                                        <th>From</th>
+                                        <td>{destination.from}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>To</th>
+                                        <td>{destination.to}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Date</th>
+                                        <td>{destination.date}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Time</th>
+                                        <td>{destination.time}</td>
+                                    </tr>
+                                </tbody>
+                            </table> 
+                            
 
+                            <div className='card text-center p-2'>
+                                <div className='row no-gutters d-flex align-items-center justify-content-center'>
 
+                                    <div className='col-lg-3'>
+                                        <img className='w-100' src={rideData.image} alt='...'/>
+                                    </div>
+                                    <div className='col-lg-3'>
+                                        <h5 className='card-title'>{rideData.rideType}</h5>
+                                    </div>
+                                   
+                                    <div className='col-lg-3'>
+                                        <h5 className='card-title'>${rideData.id}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                         </div>
+                    )}
                 </div>
+                <div className='col-lg-7'>
+    
+                    <GoogleMap/>
+                </div>
+            </div>
+        </div>
     );
 };
+
 
 export default Pick;
